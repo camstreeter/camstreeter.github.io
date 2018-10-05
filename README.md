a3f188ac6454e08467a66bb2ed9591fa4e35ce95
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-10-04 17:05:39 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# camstreeter.github.io
--- client/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-if (typeof window !== 'undefined') {
-  ReactDOM.render(<App />, document.getElementById('app'));
-}
